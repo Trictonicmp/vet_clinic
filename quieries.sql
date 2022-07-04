74dc2af996f7f7e8168a7988fe9d0c2820ba7325
Trictonicmp <code_shadow@hotmail.com>
2022-07-04 17:57:12 -0500

    Added SQL queries

---
+++ quieries.sql
@@ -0,0 +1,39 @@
+/* QUERIES */
+/* Find all animals whose name ends in "mon" */
+SELECT * FROM animals WHERE name LIKE '%mon';
+
+/* List name of all animals that where born between 
+** 2016 and 2019 */
+SELECT name FROM animals 
+  WHERE date_of_birth 
+  BETWEEN '2016-01-01' AND '2019-01-01';
+
+/* List the name of all animals that are neutered 
+** and have less than 3 scape attempts */
+SELECT name FROM animals 
+  WHERE neutered = true AND escape_attempts < 3;
+
+/* List date of birth of all animals named either 
+** "Agumon" or "pikachu" */
+SELECT date_of_birth FROM animals 
+  WHERE name = 'Agumon' OR name = 'Pikachu';
+
+/* List name and scape attempts of animals that
+** weight more than 10.5kg */
+SELECT name, escape_attempts FROM animals
+  WHERE weight_kg > 10.5;
+
+/* Find all animals that are neutered */
+SELECT * FROM animals 
+  WHERE neutered = true;
+
+/* Find all animals not named "Gabumon" */
+SELECT * FROM animals 
+  WHERE name != 'Gabumon';
+
+/* Find all animals with weight between 10.4kg and 17.3kg  
+** (including the animals with the weights that equals 
+** precisely 10.4kg or 17.3kg) */
+
+SELECT * FROM animals 
+  WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
---
+++ schema.sql
@@ -0,0 +1,16 @@
+/* Creating the database */
+CREATE DATABASE vet_clinic;
+
+/* Connecting to database */
+\c vet_clinic
+
+/* Creating animals table */
+CREATE TABLE animals (
+    id                  INT GENERATED ALWAYS AS IDENTITY,
+    name                VARCHAR(40),
+    date_of_birth      DATE,
+    escape_attempts     INT,
+    neutered            BOOLEAN,
+    weight_kg           decimal,
+    PRIMARY KEY(id)
+);
