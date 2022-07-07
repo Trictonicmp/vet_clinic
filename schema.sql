d2ab09e807dcdd90b7eaad22447f4753c55a24fd
Trictonicmp <code_shadow@hotmail.com>
2022-07-07 11:40:51 -0500

    Add visits table

--- schema.sql
+++ schema.sql
@@ -58,3 +58,10 @@ CREATE TABLE specializations (
   species_id INT REFERENCES species(id),
   PRIMARY KEY (vet_id, species_id)
 );
+
+/* Create a "join table" called visits */
+CREATE TABLE visits (
+  animal_id INT REFERENCES animals(id),
+  vet_id    INT REFERENCES vets(id),
+  PRIMARY KEY (animal_id, vet_id)
+);
